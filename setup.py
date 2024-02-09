0279125c7609f12fd0429264e240bc8a1dc87d58
pgmesa <pgmesa.sm@gmail.com>
2024-02-09 18:25:13 +0000

    Preparing for PyPI deployment

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setuptools.setup(
     version='0.1.0',
     author="Pablo García Mesa",
     author_email="pgmesa.sm@gmail.com",
-    description="A minimalistic tool for setting up Kaggle and Google Colab environments from a GitHub repository",
+    description="Minimalistic tool for easily setting up and switching between Kaggle and Google Colab environments from a GitHub repository, to leverage their GPU-accelerated hardware",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pgmesa/kaggle_colab",
+    url="https://github.com/pgmesa/kaggle-colab",
     packages=setuptools.find_packages(),
     python_requires=">=3.9",
     classifiers=[
